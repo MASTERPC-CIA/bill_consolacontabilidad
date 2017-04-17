f843bdd05cbb39a757027741b509a1987d979d22
jaimejhonle <jaimejhonle@gmail.com>
2017-04-16 23:41:44 -0500

    bug session

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -131,4 +131,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('generic_model');
+$autoload['model'] = array('Generic_model');

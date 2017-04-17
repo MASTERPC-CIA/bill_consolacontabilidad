f038489f95d62051c1e226f7079ad09c0ac879e7
jaimejhonle <jaimejhonle@gmail.com>
2017-04-16 23:33:10 -0500

    bug session

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','Session','User','encript','form_validation','javascript');
+$autoload['libraries'] = array('database','session','user','encript','form_validation','javascript');
 
 /*
 | -------------------------------------------------------------------
